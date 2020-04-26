086aed7ce03e9ce439d9b52a748e1224ee47bf17
Ged Kirkham <gedkirkham@protonmail.com>
2020-04-26 15:00:11 +0100

    create initial model designs

--- volm/app/models.py
+++ volm/app/models.py
@@ -1,3 +1,53 @@
 from django.db import models
 
-# Create your models here.
+class Advert(models.Model):
+    active
+    address
+    created
+    description
+    end_date
+    last_modified
+    last_modified_by
+    organisation
+    start_date
+    tag_line
+    title
+
+class Address(models.Model):
+    address_1
+    address_2
+    address_3
+    address_4
+    address_5
+    address_postcode
+
+class Contact(models.Model):
+    address_1
+    email_1
+    organisation
+    phone_1
+
+class Organisation(models.Model):
+    active
+    banner_image
+    created
+    charity_id
+    short_bio
+    long_bio
+    tag_line
+    last_modified
+    logo
+    name
+
+class UserProfile(models.Model):
+    organiser
+    profile_pic
+    worker
+
+class Worker(models.Model):
+    skills
+    availability_day
+    availability_time
+    travel_mode
+    travel_distance
+    remote
