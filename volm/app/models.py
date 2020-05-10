ce7921d6fd5c7405635a825a769b3c3f1a0cd156
ged <gedkirkham@protonmail.com>
2020-05-10 13:09:51 +0100

    refactor: migrate models

--- volm/app/models.py
+++ volm/app/models.py
@@ -1,53 +1,90 @@
+from django.conf import settings
 from django.db import models
+# from django.contrib.auth import get_user_model
 
 class Advert(models.Model):
-    active
-    address
-    created
-    description
-    end_date
-    last_modified
-    last_modified_by
-    organisation
-    start_date
-    tag_line
-    title
+    active = models.BooleanField(default=True)
+    address = models.ForeignKey('Address', on_delete=models.SET_NULL, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=20000)
+    end_date = models.DateTimeField()
+    modified = models.DateTimeField(auto_now=True)
+    last_modified_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+    organisation = models.ForeignKey('Organisation', on_delete=models.CASCADE)
+    start_date = models.DateTimeField()
+    tag_line = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
 
 class Address(models.Model):
-    address_1
-    address_2
-    address_3
-    address_4
-    address_5
-    address_postcode
+    active = models.BooleanField(default=True)
+    address_1 = models.CharField(max_length=100)
+    address_2 = models.CharField(max_length=100)
+    address_3 = models.CharField(max_length=100)
+    address_4 = models.CharField(max_length=100)
+    address_5 = models.CharField(max_length=100)
+    address_postcode = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    organisation = models.ForeignKey('Organisation', on_delete=models.CASCADE)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class Contact(models.Model):
-    address_1
-    email_1
-    organisation
-    phone_1
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    email_1 = models.CharField(max_length=100)
+    modified = models.DateTimeField(auto_now=True)
+    organisation = models.ForeignKey('Organisation', on_delete=models.CASCADE)
+    phone_1_ext = models.IntegerField()
+    phone_1_num = models.IntegerField()
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+def organisation_directory_path(instance, filename):
+    org = Organisation.objects.filter(user_profile=instance.user.id)
+    upload = 'org_{0}/{1}'.format(org, filename) # Will not work. Need to locate users assigned organisation
 
 class Organisation(models.Model):
-    active
-    banner_image
-    created
-    charity_id
-    short_bio
-    long_bio
-    tag_line
-    last_modified
-    logo
-    name
+    active = models.BooleanField(default=True)
+    banner_image = models.ImageField(upload_to=organisation_directory_path)
+    created =  models.DateTimeField(auto_now_add=True)
+    charity_id = models.CharField(max_length=100)
+    short_bio = models.CharField(max_length=1000)
+    long_bio = models.CharField(max_length=10000)
+    tag_line = models.CharField(max_length=100)
+    modified = models.DateTimeField(auto_now=True)
+    logo = models.ImageField(upload_to=organisation_directory_path)
+    name = models.CharField(max_length=100)
+
+class Skills(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100)
+
+class Time(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    from_time = models.IntegerField()
+    to_time = models.IntegerField()
+
+class TravelMode(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    mode = models.CharField(max_length=100)
+
+def user_directory_path(instance, filename):
+    upload - 'user_{0}/{1}'.format(instance.user.id, filename)
 
 class UserProfile(models.Model):
-    organiser
-    profile_pic
-    worker
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    organiser = models.ManyToManyField(Organisation)
+    profile_pic = models.ImageField(upload_to=user_directory_path)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    worker = models.ForeignKey('Worker', on_delete=models.CASCADE)
 
 class Worker(models.Model):
-    skills
-    availability_day
-    availability_time
-    travel_mode
-    travel_distance
-    remote
+    active = models.BooleanField()
+    availability_day = models.CharField(max_length=100)
+    availability_time = models.ManyToManyField(Time)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    skills = models.ManyToManyField(Skills)
+    travel_mode = models.ManyToManyField(TravelMode)
+    travel_distance = models.IntegerField()
+    remote = models.BooleanField()
