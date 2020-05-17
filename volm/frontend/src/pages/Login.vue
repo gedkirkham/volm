0cd0ec65439d34cda8c1a95e994a01f49ea7b206
ged <gedkirkham@protonmail.com>
2020-05-17 15:01:52 +0100

    add quasar frontend to project

---
+++ volm/frontend/src/pages/Login.vue
@@ -0,0 +1,34 @@
+<template>
+  <q-page class="flex flex-center">
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-full.svg"
+    >
+
+    Login page
+
+    <q-btn
+      label="Login"
+      @click="login()"
+    />
+  </q-page>
+</template>
+
+<script>
+export default {
+  name: 'PageIndex',
+  data () {
+    return {
+      1: 1,
+    }
+  },
+  methods: {
+    login () {
+      this
+        .$http
+        .post(process.env.API)
+        .catch(ERROR => console.error('login()', ERROR))
+    },
+  },
+}
+</script>
