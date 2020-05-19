d336cd32f354a1d87b0c488ad6c13cc9a67dc473
ged <gedkirkham@protonmail.com>
2020-05-19 19:40:15 +0100

    refactor: update models to further separate users and organisations

--- volm/app/models.py
+++ volm/app/models.py
@@ -2,20 +2,20 @@ from django.conf import settings
 from django.db import models
 # from django.contrib.auth import get_user_model
 
-class Advert(models.Model):
+class OrgAdvert(models.Model):
     active = models.BooleanField(default=True)
-    address = models.ForeignKey('Address', on_delete=models.SET_NULL, blank=True, null=True)
+    address = models.ForeignKey('UserAddress', on_delete=models.SET_NULL, blank=True, null=True)
     created = models.DateTimeField(auto_now_add=True)
     description = models.CharField(max_length=20000)
     end_date = models.DateTimeField()
     modified = models.DateTimeField(auto_now=True)
     last_modified_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
-    organisation = models.ForeignKey('Organisation', on_delete=models.CASCADE)
+    organisation = models.OneToOneField('Organisation', on_delete=models.CASCADE)
     start_date = models.DateTimeField()
     tag_line = models.CharField(max_length=100)
     title = models.CharField(max_length=100)
 
-class Address(models.Model):
+class OrgAddress(models.Model):
     active = models.BooleanField(default=True)
     address_1 = models.CharField(max_length=100)
     address_2 = models.CharField(max_length=100)
@@ -26,9 +26,8 @@ class Address(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     modified = models.DateTimeField(auto_now=True)
     organisation = models.ForeignKey('Organisation', on_delete=models.CASCADE)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
-class Contact(models.Model):
+class OrgContact(models.Model):
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
     email_1 = models.CharField(max_length=100)
@@ -36,7 +35,6 @@ class Contact(models.Model):
     organisation = models.ForeignKey('Organisation', on_delete=models.CASCADE)
     phone_1_ext = models.IntegerField()
     phone_1_num = models.IntegerField()
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 def organisation_directory_path(instance, filename):
     org = Organisation.objects.filter(user_profile=instance.user.id)
@@ -75,8 +73,28 @@ class UserProfile(models.Model):
     modified = models.DateTimeField(auto_now=True)
     organiser = models.ManyToManyField(Organisation)
     profile_pic = models.ImageField(upload_to=user_directory_path)
-    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     worker = models.ForeignKey('Worker', on_delete=models.CASCADE)
+
+class UserAddress(models.Model):
+    active = models.BooleanField(default=True)
+    address_1 = models.CharField(max_length=100)
+    address_2 = models.CharField(max_length=100)
+    address_3 = models.CharField(max_length=100)
+    address_4 = models.CharField(max_length=100)
+    address_5 = models.CharField(max_length=100)
+    address_postcode = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+
+class UserContact(models.Model):
+    active = models.BooleanField(default=True)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
+    phone_1_ext = models.IntegerField()
+    phone_1_num = models.IntegerField()
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
 
 class Worker(models.Model):
     active = models.BooleanField()
@@ -87,4 +105,17 @@ class Worker(models.Model):
     skills = models.ManyToManyField(Skills)
     travel_mode = models.ManyToManyField(TravelMode)
     travel_distance = models.IntegerField()
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     remote = models.BooleanField()
+
+class WorkerAdvert(models.Model):
+    active = models.BooleanField(default=True)
+    address = models.OneToOneField('UserAddress', on_delete=models.SET_NULL, blank=True, null=True)
+    created = models.DateTimeField(auto_now_add=True)
+    description = models.CharField(max_length=20000)
+    end_date = models.DateTimeField()
+    modified = models.DateTimeField(auto_now=True)
+    last_modified_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.SET_NULL, blank=True, null=True)
+    start_date = models.DateTimeField()
+    tag_line = models.CharField(max_length=100)
+    title = models.CharField(max_length=100)
