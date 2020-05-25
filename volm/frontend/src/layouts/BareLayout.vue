1dae5f1d62c4f43e4b6810302e019d6035c7677a
ged <gedkirkham@protonmail.com>
2020-05-25 13:32:35 +0100

    refactor: create BareLayout.vue page

---
+++ volm/frontend/src/layouts/BareLayout.vue
@@ -0,0 +1,19 @@
+<template>
+    <q-layout view="lHh Lpr lFf">
+        <header-bar />
+
+        <q-page-container>
+            <router-view />
+        </q-page-container>
+    </q-layout>
+</template>
+
+<script>
+import HeaderBar from 'src/components/HeaderBar.vue'
+export default {
+    name: 'BareLayout',
+    components: {
+        HeaderBar,
+    },
+}
+</script>
