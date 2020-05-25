dc3db2cf1b252dc7ecc1ad75e5132d0febbde175
ged <gedkirkham@protonmail.com>
2020-05-25 12:59:15 +0100

    refactor: fix all indentation to 4 spaces

--- volm/frontend/src/boot/interceptor.js
+++ volm/frontend/src/boot/interceptor.js
@@ -1,24 +1,24 @@
 import Vue from 'vue'
 
 function refreshToken () {
-  return Vue
-    .http
-    .post(process.env.API + '/token/refresh/', { refresh: localStorage.getItem('refresh') || null })
-    .then(RESP => localStorage.setItem('access', RESP.body.access))
+    return Vue
+        .http
+        .post(process.env.API + '/token/refresh/', { refresh: localStorage.getItem('refresh') || null })
+        .then(RESP => localStorage.setItem('access', RESP.body.access))
 }
 
 Vue.http.interceptors.push(request => {
-  let hasRefreshedToken = false
+    let hasRefreshedToken = false
 
-  if (request.url.includes('/api/token/refresh/')) hasRefreshedToken = true
-  else request.headers.set('Authorization', `Bearer ${localStorage.getItem('access') || null}`)
-  request.headers.set('Accept', 'application/json')
+    if (request.url.includes('/api/token/refresh/')) hasRefreshedToken = true
+    else request.headers.set('Authorization', `Bearer ${localStorage.getItem('access') || null}`)
+    request.headers.set('Accept', 'application/json')
 
-  return function (response) {
-    if (response.status === 401 && !hasRefreshedToken) {
-      return Promise
-        .resolve(refreshToken()
-          .then(_ => Vue.http(request)))
+    return function (response) {
+        if (response.status === 401 && !hasRefreshedToken) {
+            return Promise
+                .resolve(refreshToken()
+                    .then(_ => Vue.http(request)))
+        }
     }
-  }
 })
--- volm/frontend/src/pages/Login.vue
+++ volm/frontend/src/pages/Login.vue
@@ -1,38 +1,69 @@
 <template>
     <q-page class="flex flex-center">
-        <img
-            alt="Quasar logo"
-            src="~assets/quasar-logo-full.svg"
-        >
-
         <q-form @submit="login()">
-            asd
+            <q-input
+                v-model="username"
+                label="Username"
+            />
+            <q-input
+                v-model="password"
+                label="Password"
+                type="password"
+            />
+            <q-btn
+                label="Login"
+                type="submit"
+            />
         </q-form>
 
-        Login page
+        <q-btn
+            label="Get User"
+            @click="getUser()"
+        />
 
         <q-btn
-            label="Login"
-            @click="login()"
+            label="Logout"
+            @click="logout()"
         />
     </q-page>
 </template>
 
 <script>
 export default {
-  name: 'PageIndex',
-  data () {
-    return {
-      1: 1,
-    }
-  },
-  methods: {
-    login () {
-      this
-        .$http
-        .post(process.env.API)
-        .catch(ERROR => console.error('login()', ERROR))
+    name: 'PageIndex',
+    data () {
+        return {
+            password: 'tet20051702',
+            username: 'kirk4231',
+        }
     },
-  },
+    methods: {
+        getUser () {
+            console.log(localStorage.getItem('access') || null)
+            this
+                .$http
+                .get(process.env.API + '/user/')
+                .catch(ERROR => console.error('login()', ERROR))
+        },
+        login () {
+            this
+                .$http
+                .post(process.env.API + '/token/', {
+                    username: 'tet20051702',
+                    password: 'kirk4231',
+                })
+                .then(RESP => {
+                    const TOKENS = RESP.body
+                    localStorage.setItem('access', TOKENS.access)
+                    localStorage.setItem('refresh', TOKENS.refresh)
+                })
+                .catch(ERROR => console.error('login()', ERROR))
+            console.log(localStorage)
+        },
+        logout () {
+            localStorage.removeItem('access_token')
+            localStorage.removeItem('refresh_token')
+        },
+    },
 }
 </script>
