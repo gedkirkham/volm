8798a086b4f840763e6518c67c99a93ddeb9da14
ged <gedkirkham@protonmail.com>
2020-05-25 13:32:07 +0100

    refactor: create HeaderBar.vue component

--- volm/frontend/src/layouts/MainLayout.vue
+++ volm/frontend/src/layouts/MainLayout.vue
@@ -1,23 +1,6 @@
 <template>
     <q-layout view="lHh Lpr lFf">
-        <q-header elevated>
-            <q-toolbar>
-                <q-btn
-                    flat
-                    dense
-                    round
-                    icon="menu"
-                    aria-label="Menu"
-                    @click="leftDrawerOpen = !leftDrawerOpen"
-                />
-
-                <q-toolbar-title>
-                    Quasar App
-                </q-toolbar-title>
-
-                <div>Quasar v{{ $q.version }}</div>
-            </q-toolbar>
-        </q-header>
+        <header-bar />
 
         <q-drawer
             v-model="leftDrawerOpen"
@@ -48,12 +31,14 @@
 
 <script>
 import EssentialLink from 'components/EssentialLink'
+import HeaderBar from 'components/HeaderBar.vue'
 
 export default {
     name: 'MainLayout',
 
     components: {
         EssentialLink,
+        HeaderBar,
     },
 
     data () {
