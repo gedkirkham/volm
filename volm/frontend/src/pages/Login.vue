9a7e271e63a556b78f59f2ce600471f23f57683c
ged <gedkirkham@protonmail.com>
2020-05-26 19:27:19 +0100

    refactor: move login API to api.js and add some styling

--- volm/frontend/src/pages/Login.vue
+++ volm/frontend/src/pages/Login.vue
@@ -1,46 +1,45 @@
 <template>
     <q-page class="flex flex-center">
-        <q-form @submit="login()">
-            <q-input
-                v-model="username"
-                label="Username"
-            />
-            <q-input
-                v-model="password"
-                label="Password"
-                type="password"
-            />
-            <q-btn
-                label="Login"
-                type="submit"
-            />
-        </q-form>
+        <div class="column items-start">
+            <q-form @submit="login()">
+                <q-input
+                    v-model="email"
+                    class="q-pb-md"
+                    label="Email"
+                    outlined
+                />
+                <q-input
+                    v-model="password"
+                    class="q-pb-md"
+                    label="Password"
+                    type="password"
+                    outlined
+                />
+
+                <q-btn
+                    color="primary"
+                    label="Login"
+                    type="submit"
+                />
+            </q-form>
+        </div>
     </q-page>
 </template>
 
 <script>
+import { loginApi } from 'src/api/auth.js'
+
 export default {
     name: 'LoginPage',
     data () {
         return {
+            email: '',
             password: '',
-            username: '',
         }
     },
     methods: {
         login () {
-            this
-                .$http
-                .post(process.env.API + '/token/', {
-                    username: '',
-                    password: '',
-                })
-                .then(RESP => {
-                    const TOKENS = RESP.body
-                    localStorage.setItem('access', TOKENS.access)
-                    localStorage.setItem('refresh', TOKENS.refresh)
-                })
-                .catch(ERROR => console.error('login()', ERROR))
+            loginApi({ email: this.email, password: this.password })
         },
     },
 }
