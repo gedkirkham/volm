59c3f9068fe602c425b8ecca5bbeaeb0d4232364
ged <gedkirkham@protonmail.com>
2020-06-01 20:41:38 +0100

    feat: add custom validity checks for first_name and last_name on RegistrationSerializer
    added because User model has first_name and last_name as Blank=True by default.

--- volm/app/serializers.py
+++ volm/app/serializers.py
@@ -3,6 +3,8 @@ from django.contrib.auth.models import User
 from rest_framework import serializers
 
 class RegistrationSerializer(serializers.ModelSerializer):
+    first_name = serializers.CharField(max_length=30)
+    last_name = serializers.CharField(max_length=150)
     class Meta:
         model = User
         fields = ['email', 'first_name', 'last_name', 'password', 'username', ]
