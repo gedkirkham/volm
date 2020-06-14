0c6f64fc0a16e0a5f1fa82df028eb0e84b3134da
ged <gedkirkham@protonmail.com>
2020-06-14 16:53:04 +0100

    style: center confirm_email page contents

--- volm/frontend/src/pages/registration/ConfirmEmail.vue
+++ volm/frontend/src/pages/registration/ConfirmEmail.vue
@@ -1,23 +1,20 @@
 <template>
-    <div class="row justify-center" style="margin-top: 25vh">
-        <q-card
-            class="my-card col"
-            bordered
-            flat
-        >
-            <q-card-section class="bg-primary text-white">
-                <div class="text-h4">Registration successful</div>
-            </q-card-section>
+    <q-page class="flex flex-center">
+        <div class="column items-start">
+            <q-card
+                bordered
+                flat
+            >
+                <q-card-section class="bg-primary text-white">
+                    <div class="text-h4">
+                        Registration successful
+                    </div>
+                </q-card-section>
 
-            <q-card-section>
-                A confirmation email has been sent to your email address.
-            </q-card-section>
-        </q-card>
-    </div>
+                <q-card-section>
+                    A confirmation email has been sent to your email address.
+                </q-card-section>
+            </q-card>
+        </div>
+    </q-page>
 </template>
-
-<style lang="sass" scoped>
-.my-card
-    width: 100%
-    max-width: 450px
-</style>
