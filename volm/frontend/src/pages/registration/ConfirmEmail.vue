f9efe7b6e4740a8fe2d0019f774fff8ea736836b
ged <gedkirkham@protonmail.com>
2020-06-14 16:19:20 +0100

    feat: create ConfirmEmail.vue template

---
+++ volm/frontend/src/pages/registration/ConfirmEmail.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="row justify-center" style="margin-top: 25vh">
+        <q-card
+            class="my-card col"
+            bordered
+            flat
+        >
+            <q-card-section class="bg-primary text-white">
+                <div class="text-h4">Registration successful</div>
+            </q-card-section>
+
+            <q-card-section>
+                A confirmation email has been sent to your email address.
+            </q-card-section>
+        </q-card>
+    </div>
+</template>
+
+<style lang="sass" scoped>
+.my-card
+    width: 100%
+    max-width: 450px
+</style>
