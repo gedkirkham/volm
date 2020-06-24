5da61b6f08f529f2b432aeb58d48e0bf47888d95
ged <gedkirkham@protonmail.com>
2020-06-24 20:01:50 +0100

    test: add test to ensure getErrorMessage returns correct values

--- volm/frontend/test/jest/__tests__/pages/registration/Form.spec.js
+++ volm/frontend/test/jest/__tests__/pages/registration/Form.spec.js
@@ -3,12 +3,21 @@
  * @jest-environment jsdom
  */
 
-import { mount, createLocalVue, shallowMount } from '@vue/test-utils'
+import {
+    createLocalVue,
+    enableAutoDestroy,
+    mount,
+    shallowMount,
+} from '@vue/test-utils'
 import Form from 'src/pages/registration/Form.vue'
 import * as All from 'quasar'
 import VueRouter from 'vue-router'
+import VueI18n from 'vue-i18n'
 // import langEn from 'quasar/lang/en-us' // change to any language you wish! => this breaks wallaby :(
 const { Quasar, date } = All
+
+import constants from 'src/constants.js'
+import messages from 'src/i18n'
 
 const components = Object.keys(All).reduce((object, key) => {
   const val = All[key]
@@ -18,29 +27,53 @@ const components = Object.keys(All).reduce((object, key) => {
   return object
 }, {})
 
+enableAutoDestroy(afterEach)
+
 describe('Registration From', () => {
     const localVue = createLocalVue()
     localVue.use(Quasar, { components }) // , lang: langEn
     localVue.use(VueRouter)
+    localVue.use(VueI18n)
 
     const router = new VueRouter()
 
+    const i18n = new VueI18n({
+        locale: 'en-us',
+        fallbackLocale: 'en-us',
+        messages,
+    })
+
     const wrapper = shallowMount(Form, {
+        i18n,
         localVue,
         router,
     })
     const vm = wrapper.vm
 
     it('passes the sanity check and creates a wrapper', () => {
-        expect(wrapper).toBeTruthy()
+        expect(wrapper.exists()).toBe(true)
     })
 
-    it.only('renders correctly', () => {
+    it('renders correctly', () => {
         expect(wrapper).toMatchSnapshot()
     })
 
-    it('has a created hook', () => {
-        expect(typeof vm.increment).toBe('function')
+    describe('methods', () => {
+        describe('getErrorMessage()', () => {
+            it('is a function', () => {
+                expect(typeof vm.getErrorMessage).toBe('function')
+            })
+
+            it('returns correct error message', () => {
+                const ERROR = wrapper.vm.getErrorMessage(constants.django.errors.blank)
+                expect(ERROR).toBe(i18n.t('pages.register.errors.blank'))
+            })
+
+            it('returns default error message', () => {
+                const ERROR = wrapper.vm.getErrorMessage('random error message')
+                expect(ERROR).toBe(i18n.t('pages.register.errors.field_contains_errors'))
+            })
+        })
     })
 
     it('accesses the shallowMount', () => {
