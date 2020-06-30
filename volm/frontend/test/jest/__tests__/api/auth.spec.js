0c9071e078d418b182c1bcf1e6b9edf4f38f6e54
ged <gedkirkham@protonmail.com>
2020-06-30 20:30:48 +0100

    test: add test to ensure that a Promise is returned if Promise is yet to be resolved from registerApi()

--- volm/frontend/test/jest/__tests__/api/auth.spec.js
+++ volm/frontend/test/jest/__tests__/api/auth.spec.js
@@ -2,8 +2,26 @@
 /**
  * @jest-environment jsdom
  */
+// import Vue from 'vue'
+import Vue from 'vue';
 
 import { registerApi } from 'src/api/auth.js'
+
+jest.mock('vue', () => ({
+    http: {
+        post: jest.fn(),
+        finally: jest.fn(),
+    }
+}))
+
+const PAY_LOAD = {
+    email: '',
+    first_name: '',
+    last_name: '',
+    password: '',
+    password_2: '',
+    username: '',
+}
 
 describe('api/auth.js ', () => {
     describe('registerApi()', () => {
@@ -15,7 +33,15 @@ describe('api/auth.js ', () => {
         it('calls a REST POST method with the correct params', () => {})
         it('sets loading property to true', () => {})
         it('sets loading property to false once resolved', () => {})
-        it('returns promise if currently "loading"', () => {})
+
+        it('returns Promise if Promise is yet to be resolved', async () => {
+            Vue.http.post.mockImplementation(() => Promise.resolve(Promise))
+            const PROMISE = registerApi(PAY_LOAD)
+            await registerApi(PAY_LOAD)
+            expect(Vue.http.post).toBeCalledTimes(1)
+            expect(PROMISE instanceof Promise).toBe(true)
+        })
+
         it('throws error if not all object properties are present', () => {})
     })
 })
