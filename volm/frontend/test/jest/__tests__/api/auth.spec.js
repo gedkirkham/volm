e51d96e7e383ca27fdfb93f8369d1b90ee75ec97
ged <gedkirkham@protonmail.com>
2020-07-05 15:37:26 +0100

    test: add test for registerApi to enure method is called correctly with the correct params

--- volm/frontend/test/jest/__tests__/api/auth.spec.js
+++ volm/frontend/test/jest/__tests__/api/auth.spec.js
@@ -15,24 +15,47 @@ jest.mock('vue', () => ({
 }))
 
 const PAY_LOAD = {
-    email: '',
-    first_name: '',
-    last_name: '',
-    password: '',
-    password_2: '',
-    username: '',
+    email: 'test@test.com',
+    first_name: 'John',
+    last_name: 'Moore',
+    password_1: 'password123',
+    password_2: 'password1234',
 }
 
 describe('api/auth.js ', () => {
+    beforeAll(() => {
+        process.env.API = 'http://test.com'
+    })
+
+    beforeEach(() => {
+        jest.resetAllMocks()
+    })
+
     describe('registerApi()', () => {
         it('is a function', () => {
             expect(typeof registerApi).toBe('function')
         })
 
-        it('calls a REST POST method', () => {})
-        it('calls a REST POST method with the correct params', () => {})
-        it('sets loading property to true', () => {})
-        it('sets loading property to false once resolved', () => {})
+        it('calls a REST POST method', async () => {
+            Vue.http.post.mockImplementation(() => Promise.resolve(Promise))
+            expect(Vue.http.post).toBeCalledTimes(0)
+            await registerApi(PAY_LOAD)
+            expect(Vue.http.post).toBeCalledTimes(1)
+        })
+
+        it('calls a REST POST method with the correct params', async () => {
+            Vue.http.post.mockImplementation(() => Promise.resolve(Promise))
+            expect(Vue.http.post).toBeCalledTimes(0)
+            await registerApi(PAY_LOAD)
+            expect(Vue.http.post).toBeCalledWith(process.env.API + '/register/', {
+                email: PAY_LOAD.email,
+                first_name: PAY_LOAD.first_name,
+                last_name: PAY_LOAD.last_name,
+                password: PAY_LOAD.password_1,
+                password_2: PAY_LOAD.password_2,
+                username: PAY_LOAD.email,
+            })
+        })
 
         it('returns Promise if Promise is yet to be resolved', async () => {
             Vue.http.post.mockImplementation(() => Promise.resolve(Promise))
@@ -41,7 +64,5 @@ describe('api/auth.js ', () => {
             expect(Vue.http.post).toBeCalledTimes(1)
             expect(PROMISE instanceof Promise).toBe(true)
         })
-
-        it('throws error if not all object properties are present', () => {})
     })
 })
