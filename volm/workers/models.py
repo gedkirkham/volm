6b4fb01ea0ca956828ac5902af4ce4aca8f17272
ged <gedkirkham@protonmail.com>
2020-11-15 19:31:09 +0000

    refactor: add worker models to models app

--- volm/workers/models.py
+++ volm/workers/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+from contact.models import Address, Contact
+from orgs.models import Org
 
-# Create your models here.
+User = get_user_model()
+
+class worker(models.Model):
+    active = models.BooleanField(default=True)
+    address = models.ForeignKey(Address, on_delete=models.SET_NULL, related_name='address')
+    contact = models.ForeignKey(Contact, on_delete=models.SET_NULL, related_name='contact details')
+    orgs = models.ManyToManyField(Org, on_delete=models.SET_NULL, related_name='associated organisation')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user account')
