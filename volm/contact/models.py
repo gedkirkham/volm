1bee64183b487f4a3962bed3bc128e45c4c3fa99
ged <gedkirkham@protonmail.com>
2020-11-16 20:45:18 +0000

    refactor: add fields to Contact and Address model

--- volm/contact/models.py
+++ volm/contact/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 class Address(models.Model):
-    pass
+    postcode = models.CharField(max_length=20, blank=True)
 
-class Contact(models.Model):
-    pass
+    class Meta:
+        verbose_name_plural = 'Address'
+
+class ContactInfo(models.Model):
+    phone_1 = models.CharField(max_length=20, blank=True)
+
+    class Meta:
+        verbose_name_plural = 'Contact Info'
