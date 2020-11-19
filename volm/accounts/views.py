27c18efa39899ec6a30e6db1d4a64e87f3921bcc
ged <gedkirkham@protonmail.com>
2020-11-19 19:10:11 +0000

    refactor: update signup template

--- volm/accounts/views.py
+++ volm/accounts/views.py
@@ -1,10 +1,9 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-
-from . import forms
+from .forms import UserSignUpForm
 
 class SignUp(CreateView):
-    form_class = forms.UserSignUpForm
+    form_class = UserSignUpForm
     success_url = reverse_lazy('login')
     template_name = 'accounts/signup.html'
