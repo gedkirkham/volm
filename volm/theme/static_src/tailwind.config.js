540e0821cb1a9a31be938fb01eb40f8a2a21a7b8
ged <gedkirkham@protonmail.com>
2020-11-24 19:32:11 +0000

    refactor: add all colours to tailwindcss

--- volm/theme/static_src/tailwind.config.js
+++ volm/theme/static_src/tailwind.config.js
@@ -1,3 +1,6 @@
+
+const colors = require('tailwindcss/colors')
+
 module.exports = {
     purge: {
         enabled: process.env.NODE_ENV === 'production',
@@ -9,4 +12,33 @@ module.exports = {
     prefix: 'tw-',
     variants: {},
     plugins: [],
+    theme: {
+        colors: {
+            white: '#fff',
+            black: '#000',
+            
+            blueGray: colors.blueGray,
+            coolGray: colors.coolGray,
+            gray: colors.gray,
+            trueGray: colors.trueGray,
+            warmGray: colors.warmGray,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            lightBlue: colors.lightBlue,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose,
+        }
+    }
 }
