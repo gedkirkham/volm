2c7a90bb21c41cbe7532fa0b255abd7a038fd68f
ged <gedkirkham@protonmail.com>
2020-12-14 20:36:02 +0000

    feat: add country and city fields to address table

--- volm/contact/models.py
+++ volm/contact/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 
 class Address(models.Model):
+    city = models.CharField(max_length=100, blank=True)
+    country = models.CharField(max_length=100, blank=True)
     postcode = models.CharField(max_length=20, blank=True)
 
     class Meta:
