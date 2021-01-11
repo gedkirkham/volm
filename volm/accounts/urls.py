eae13709e3b43b796040943398d00296d3080547
ged <gedkirkham@protonmail.com>
2021-01-11 20:13:08 +0000

    feat: create profile view

--- volm/accounts/urls.py
+++ volm/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
-from .views import SignUp
+from .views import SignUp, ProfileView
 
 app_name = 'accounts'
 
@@ -8,4 +8,5 @@ urlpatterns = [
     path('login/', auth_views.LoginView.as_view(template_name='accounts/login.html'), name="login"),
     path('logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('signup/', SignUp.as_view(), name="signup"),
+    path('profile/', ProfileView.as_view(), name="profile"),
 ]
--- volm/accounts/views.py
+++ volm/accounts/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView
+from django.views.generic import CreateView, TemplateView
+from django.contrib.auth.mixins import LoginRequiredMixin
+
 from .forms import UserSignUpForm
+
+class ProfileView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/profile_base.html'
 
 class SignUp(CreateView):
     form_class = UserSignUpForm
