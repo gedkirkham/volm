53dd6f43ce90d98e7b4b3768b2b711d184249960
ged <gedkirkham@protonmail.com>
2021-01-11 22:11:54 +0000

    feat: add workers adverts to their profile page

--- volm/accounts/views.py
+++ volm/accounts/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, TemplateView
+from django.views.generic import CreateView, ListView, TemplateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import UserSignUpForm
+from workers.models import Worker
 
-class ProfileView(LoginRequiredMixin, TemplateView):
+class ProfileView(LoginRequiredMixin, ListView):
     template_name = 'accounts/profile_base.html'
+
+    def get_queryset(self):
+        return Worker.objects.filter(active=True, user=self.request.user).order_by('-created')
 
 class SignUp(CreateView):
     form_class = UserSignUpForm
