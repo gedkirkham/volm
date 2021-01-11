e45e0d5b0bdf8c48a6c8415306c97c0f1ea0feb9
ged <gedkirkham@protonmail.com>
2021-01-11 20:10:18 +0000

    refactor: hide availability form from workers view

--- volm/workers/views.py
+++ volm/workers/views.py
@@ -30,7 +30,7 @@ class WorkerDeleteView(LoginRequiredMixin, DeleteView):
     model = Worker
 
 class WorkerCreateView(LoginRequiredMixin, TemplateView):
-    availability_form_class = AvailabilityFormSet
+    # availability_form_class = AvailabilityFormSet
     worker_form_class = WorkerForm
     login_url = '/login/'
     template_name = 'workers/worker_update_create_form.html'
@@ -48,25 +48,27 @@ class WorkerCreateView(LoginRequiredMixin, TemplateView):
             worker.contact = ContactInfo.objects.get(user=request.user)
             worker.save()
 
-        availability_form = self.availability_form_class(post_data, initial=initial, queryset=Availability.objects.filter(worker=worker))
-        context = self.get_context_data(worker_form=worker_form, availability_form=availability_form)
+        # availability_form = self.availability_form_class(post_data, initial=initial, queryset=Availability.objects.filter(worker=worker))
+        # context = self.get_context_data(worker_form=worker_form, availability_form=availability_form)
+        context = self.get_context_data(worker_form=worker_form)
 
-        if availability_form.is_valid():
-            for form in availability_form:
-                availability = form.save(commit=False)
-                availability.worker = worker
-                availability.save()
+        # if availability_form.is_valid():
+        #     for form in availability_form:
+        #         availability = form.save(commit=False)
+        #         availability.worker = worker
+        #         availability.save()
 
-                for day in form.cleaned_data.get('day'):
-                    availability.day.add(day)
+        #         for day in form.cleaned_data.get('day'):
+        #             availability.day.add(day)
                     
-                for time_from in form.cleaned_data.get('time_from'):
-                    availability.time_from.add(time_from)
+        #         for time_from in form.cleaned_data.get('time_from'):
+        #             availability.time_from.add(time_from)
 
-                for time_to in form.cleaned_data.get('time_to'):
-                    availability.time_to.add(time_to)
+        #         for time_to in form.cleaned_data.get('time_to'):
+        #             availability.time_to.add(time_to)
 
-        if worker_form.is_valid() and availability_form.is_valid():
+        # if worker_form.is_valid() and availability_form.is_valid():
+        if worker_form.is_valid():
             return redirect(reverse_lazy('workers:worker_detail', kwargs={ 'pk': worker.pk }))
 
         return self.render_to_response(context)
